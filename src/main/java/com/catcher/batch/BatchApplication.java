2b977c40aadfa1d3e307bf4a24f20fc864be5d0e
HongGeun <ghdrms163@gmail.com>
2023-10-12 03:19:39 +0900

    chore: feign 의존성 및 config 추가

--- src/main/java/com/catcher/batch/BatchApplication.java
+++ src/main/java/com/catcher/batch/BatchApplication.java
@@ -2,10 +2,12 @@ package com.catcher.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@EnableFeignClients(basePackages = "com.catcher.batch.resource.external")
 public class BatchApplication {
 
     public static void main(String[] args) {
