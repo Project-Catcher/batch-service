aa7dc58cf983ec3c0bcf44b6c8dae7839db11249
권덕주 <djgwon@lifegoeson.kr>
2023-10-13 01:30:00 +0900

    fix: 중복 KmsUtils 중 1개 삭제

--- src/main/java/com/catcher/batch/infrastructure/utils/KmsUtils.java
+++ src/main/java/com/catcher/batch/infrastructure/utils/KmsUtils.java
@@ -22,7 +22,7 @@ public class KmsUtils {
     @Value("${aws.kms.keyId}")
     private static String KEY_ID;
 
-    @Value("${spring.profiles.active}")
+    @Value("${spring.config.activate.on-profile}")
     private static String PROFILE;
 
     public static String encrypt(String text) {
