f5b3817fa946a4868f2a98ec33fce057f8cf1019
inyoung <charmingyear@gmail.com>
2023-11-08 01:53:46 +0900

    fix: test 코드에 MockBean 추가

--- src/test/java/com/catcher/batch/resource/CampingControllerTest.java
+++ src/test/java/com/catcher/batch/resource/CampingControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +21,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = CampingController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureMockMvc
 class CampingControllerTest {
 
