e0fe9097b3aa4e963281b80eefda0efd5462adde
inyoung <charmingyear@gmail.com>
2023-11-08 00:33:59 +0900

    feat: camping feign 호출

---
+++ src/main/java/com/catcher/batch/core/dto/CampingApiResponse.java
@@ -0,0 +1,74 @@
+package com.catcher.batch.core.dto;
+
+import com.catcher.batch.annotation.CatcherJson;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@CatcherJson(path = "response.body")
+public class CampingApiResponse {
+
+    @JsonProperty("items")
+    private CampingItems items;
+
+    @JsonProperty("totalCount")
+    private Integer totalCount;
+
+    @JsonProperty("numOfRows")
+    private Integer numOfRows;
+
+    @JsonProperty("pageNo")
+    private Integer pageNo;
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CampingItems {
+        @JsonProperty("item")
+        private List<CampingItem> item;
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class CampingItem {
+
+        @JsonProperty("facltNm")
+        private String name;
+
+        @JsonProperty("contentId")
+        private String key;
+
+        @JsonProperty("lineIntro")
+        private String description;
+
+        @JsonProperty("doNm")
+        private String province;
+
+        @JsonProperty("sigunguNm")
+        private String city;
+
+        @JsonProperty("addr1")
+        private String address;
+
+        @JsonProperty("induty")
+        private String category;
+
+        @JsonProperty("zipcode")
+        private String zipcode;
+
+        @JsonProperty("homepage")
+        private String homepage;
+
+        @JsonProperty("mapX")
+        private String mapX;
+
+        @JsonProperty("mapY")
+        private String mapY;
+
+        @JsonProperty("firstImageUrl")
+        private String thumbnailUrl;
+    }
+}
---
+++ src/main/java/com/catcher/batch/resource/CampingController.java
@@ -0,0 +1,48 @@
+package com.catcher.batch.resource;
+
+import com.catcher.batch.common.response.CommonResponse;
+import com.catcher.batch.common.service.CatcherFeignService;
+import com.catcher.batch.core.domain.CommandExecutor;
+import com.catcher.batch.core.domain.command.RegisterCampingDataCommand;
+import com.catcher.batch.core.dto.CampingApiResponse;
+import com.catcher.batch.core.service.CampingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/camping")
+public class CampingController {
+    private final CatcherFeignService catcherFeignService;
+    private final CampingService campingService;
+    private final CommandExecutor commandExecutor;
+
+    @GetMapping("/feign-batch")
+    public CommonResponse<CampingApiResponse> getCampingDataByFeign(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "5") Integer count
+    ) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("pageNo", page);
+        params.put("numOfRows", count);
+        CampingApiResponse campingApiResponse = catcherFeignService.parseService(params, CampingApiResponse.class);
+
+        return CommonResponse.success(200, campingApiResponse);
+    }
+
+    @PostMapping("/batch")
+    public CommonResponse<Object> batchCampingData(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "1") Integer count
+    ) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("pageNo", page);
+        params.put("numOfRows", count);
+        CampingApiResponse campingApiResponse = catcherFeignService.parseService(params, CampingApiResponse.class);
+
+        commandExecutor.run(new RegisterCampingDataCommand(campingService, campingApiResponse));
+        return CommonResponse.success(201, null);
+    }
+}
