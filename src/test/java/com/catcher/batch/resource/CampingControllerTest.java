39b9957715c6a63fc5193a679705cbc9fbd2e147
inyoung <charmingyear@gmail.com>
2023-11-08 02:18:59 +0900

    fix: @SpringBootTest 제거

--- src/test/java/com/catcher/batch/resource/CampingControllerTest.java
+++ src/test/java/com/catcher/batch/resource/CampingControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = CampingController.class)
+@WebMvcTest(value = CampingController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureMockMvc
 class CampingControllerTest {
