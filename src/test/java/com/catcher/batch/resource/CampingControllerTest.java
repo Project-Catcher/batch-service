386ce4cc9af7c10d35f56003f065727c45c50c8d
inyoung <charmingyear@gmail.com>
2023-11-08 02:20:25 +0900

    fix: security 비활성화

--- src/test/java/com/catcher/batch/resource/CampingControllerTest.java
+++ src/test/java/com/catcher/batch/resource/CampingControllerTest.java
@@ -7,6 +7,7 @@ import com.catcher.batch.core.service.CampingService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,7 +21,9 @@ import java.util.HashMap;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(value = CampingController.class)
+@WebMvcTest(
+        value = CampingController.class,
+        excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureMockMvc
 class CampingControllerTest {
