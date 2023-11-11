f7d11edc2e2431da0052e1c8f634548c69f8a400
HongGeun <ghdrms163@gmail.com>
2023-11-11 14:13:52 +0900

    add regions and apply ssh tunneling

--- src/main/java/com/catcher/batch/datasource/config/DatabaseConfiguration.java
+++ src/main/java/com/catcher/batch/datasource/config/DatabaseConfiguration.java
@@ -53,32 +53,27 @@ public class DatabaseConfiguration {
     @Bean
     public DataSource dataSource() throws Exception {
         // KMS 활용한 연결
-//        JSch jsch = new JSch();
-//        Session session = jsch.getSession(
-//                KmsUtils.decrypt(sshUsername),
-//                KmsUtils.decrypt(sshHost),
-//                sshPort
-//        );
-//        session.setPassword(KmsUtils.decrypt(sshPassword));
-//        session.setConfig("StrictHostKeyChecking", "no");
-//        session.connect();
-//
-//        int assignedPort = session.setPortForwardingL(0,
-//                KmsUtils.decrypt(originUrl),
-//                localPort
-//        ); // TODO: lport 값(현재 0)은 추후 서버 올릴때는 지정해줘야함
-//
-//        return DataSourceBuilder.create()
-//                .url(KmsUtils.decrypt(databaseUrl).replace(Integer.toString(localPort), Integer.toString(assignedPort)))
-//                .username(KmsUtils.decrypt(databaseUsername))
-//                .password(KmsUtils.decrypt(databasePassword))
-//                .build();
-        // EKS ConfigMap & Secret
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(
+                KmsUtils.decrypt(sshUsername),
+                KmsUtils.decrypt(sshHost),
+                sshPort
+        );
+        session.setPassword(KmsUtils.decrypt(sshPassword));
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect();
+
+        int assignedPort = session.setPortForwardingL(0,
+                KmsUtils.decrypt(originUrl),
+                localPort
+        ); // TODO: lport 값(현재 0)은 추후 서버 올릴때는 지정해줘야함
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(databaseUrl);
-        dataSource.setUsername(databaseUsername);
-        dataSource.setPassword(databasePassword);
+        dataSource.setUrl(KmsUtils.decrypt(databaseUrl).replace(Integer.toString(localPort), Integer.toString(assignedPort)));
+        dataSource.setUsername(KmsUtils.decrypt(databaseUsername));
+        dataSource.setPassword(KmsUtils.decrypt(databasePassword));
+
         return dataSource;
     }
 }
