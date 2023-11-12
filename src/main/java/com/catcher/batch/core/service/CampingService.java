1bbecf7f0de75af0a10350759c06e6deca02074d
inyoung <charmingyear@gmail.com>
2023-11-13 02:32:01 +0900

    refactor: stream으로 리팩토링

--- src/main/java/com/catcher/batch/core/service/CampingService.java
+++ src/main/java/com/catcher/batch/core/service/CampingService.java
@@ -12,8 +12,8 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,30 +29,23 @@ public class CampingService {
                 .orElseGet(() -> categoryRepository.save(Category.create(CATEGORY_NAME)));
 
         List<CampingApiResponse.CampingItem> campingItems = campingApiResponse.getItems().getItem();
-        List<CatcherItem> catcherItems = new ArrayList<>();
 
-        for (CampingApiResponse.CampingItem campingItem : campingItems) {
-            Location location = getLocationByDescription(campingItem.getProvince(), campingItem.getCity());
+        List<CatcherItem> catcherItems = campingItems.stream()
+                .filter(campingItem -> !isDuplicateHashValue(hashString(campingItem.getKey())))
+                .map(campingItem -> {
+                    Location location = getLocationByDescription(campingItem.getProvince(), campingItem.getCity());
+                    String hashKey = hashString(campingItem.getKey());
 
-            String hashKey = hashString(campingItem.getKey());
-
-            // 중복 해시값 체크
-            if (isDuplicateHashValue(hashKey)) {
-                System.out.println("중복됨");
-                continue;
-            }
-
-            CatcherItem catcherItem = CatcherItem.builder()
-                    .category(category)
-                    .location(location)
-                    .title(campingItem.getName())
-                    .description(campingItem.getDescription())
-                    .thumbnailUrl(campingItem.getThumbnailUrl())
-                    .itemHashValue(hashKey)
-                    .build();
-
-            catcherItems.add(catcherItem);
-        }
+                    return CatcherItem.builder()
+                            .category(category)
+                            .location(location)
+                            .title(campingItem.getName())
+                            .description(campingItem.getDescription())
+                            .thumbnailUrl(campingItem.getThumbnailUrl())
+                            .itemHashValue(hashKey)
+                            .build();
+                })
+                .collect(Collectors.toList());
 
         catcherItemRepository.saveAll(catcherItems);
     }
@@ -64,7 +57,6 @@ public class CampingService {
                 .orElseThrow();
     }
 
-    // TODO : 중복체크가 안됨 ㅜ.ㅜ
     private boolean isDuplicateHashValue(String hashKey) {
         return catcherItemRepository.findByItemHashValue(hashKey).isPresent();
     }
