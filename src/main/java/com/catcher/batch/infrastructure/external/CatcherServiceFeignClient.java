07199cb058742f48bc8b1290de93597b85d44c30
inyoung <charmingyear@gmail.com>
2023-11-25 03:12:26 +0900

    feat: internal api 적용

---
+++ src/main/java/com/catcher/batch/infrastructure/external/CatcherServiceFeignClient.java
@@ -0,0 +1,15 @@
+package com.catcher.batch.infrastructure.external;
+
+import com.catcher.batch.common.response.CommonResponse;
+import com.catcher.batch.infrastructure.external.response.InternalAddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "catcher-service", url = "${url.catcher-service}")
+public interface CatcherServiceFeignClient {
+
+    @GetMapping("/internal/address/{query}")
+    CommonResponse<InternalAddressResponse> getAddressByQuery(@PathVariable String query);
+
+}
