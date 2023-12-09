736773ac6e50b19e2db8141d735c905f8e47f84c
권덕주 <djgwon@lifegoeson.kr>
2023-12-09 21:17:24 +0900

    Revert "test: db health check용 API 생성"
    This reverts commit e12f7e1a71d690869d52597ced4391c8393ad254.

--- src/main/java/com/catcher/batch/resource/HealthCheckController.java
+++ src/main/java/com/catcher/batch/resource/HealthCheckController.java
@@ -1,8 +1,6 @@
 package com.catcher.batch.resource;
 
-import com.catcher.batch.core.database.LocationRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/health")
-@Slf4j
 public class HealthCheckController {
-
-    private final LocationRepository locationRepository;
 
     @GetMapping
     public ResponseEntity<String> healthCheck(){
-        return ResponseEntity.ok().body("ok");
-    }
-
-    /* db 접근 체크용 */
-    @GetMapping("/db")
-    public ResponseEntity<String> healthCheckWithDB() {
-        final var result = locationRepository.findByAreaCode("11"); // 임의의 DB 쿼리
-        log.info("result.isPresent() : {}", result.isPresent());
         return ResponseEntity.ok().body("ok");
     }
 }
